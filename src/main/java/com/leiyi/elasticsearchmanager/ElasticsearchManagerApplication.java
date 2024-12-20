25b1a2d8eccb42a340e795e4bad90f3fbc392eb5
leiyi2 <leiyi2@tuhu.cn>
2024-12-20 18:26:30 +0800

    init project

--- src/main/java/com/leiyi/elasticsearchmanager/ElasticsearchManagerApplication.java
+++ src/main/java/com/leiyi/elasticsearchmanager/ElasticsearchManagerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ElasticsearchManagerApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ElasticsearchManagerApplication.class, args);
     }
 
