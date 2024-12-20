b087d95747e2db844a7d250deca6ca46610e26ac
leiyi2 <leiyi2@tuhu.cn>
2024-12-20 18:26:12 +0800

    init project

---
+++ src/test/java/com/leiyi/elasticsearchmanager/ElasticsearchManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leiyi.elasticsearchmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
